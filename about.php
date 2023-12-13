67d4cf9ba3f9de76145e8d972470fae34548b535
Danish Sayed <110024428+danishxsayed@users.noreply.github.com>
2023-12-13 15:50:14 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    
+<!-- header section starts -->
+
+<section class="header">
+
+  <a href="home.php" class="logo">travel.</a>
+
+  <nav class="navbar">
+    <a href="home.php">home</a>
+    <a href="about.php">about</a>
+    <a href="package.php">package</a>
+    <a href="book.php">book</a>
+  </nav>
+
+  <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/heading-1.jpg) no-repeat">
+    <h1>about us</h1>
+</div>
+
+<!-- about section starts -->
+
+<section class="about">
+
+  <div class="image">
+    <img src="images/about-img.jpg" alt="">
+  </div>
+
+  <div class="content">
+    <h3>why choose us?</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quidem hic
+     repellendus expedita iure pariatur laborum quis, voluptatibus repellat labore.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita repellat 
+     illo totam voluptatibus llenduse quis a mollitia in aut beatae asperiores, 
+     mollitia dolor, corporis consectetur autem! Quam?</p>
+     <div class="icons-container">
+        <div class="icons">
+          <i class="fas fa-map"></i>
+          <span>top destinations</span>
+        </div>
+        <div class="icons">
+          <i class="fas fa-hand-holding-usd"></i>
+          <span>affordable price</span>
+        </div>
+        <div class="icons">
+          <i class="fas fa-headset"></i>
+          <span>24/7 guide service</span>
+        </div>
+     </div>
+  </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- reviews section starts -->
+
+<section class="reviews">
+
+  <h1 class="heading-title"> clients reviews </h1>
+
+  <div class="swiper reviews-slider">
+
+    <div class="swiper-wrapper">
+
+      <div class="swiper-slide slide">
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+          Voluptatem aliquam ratione mollitia deserunt similique 
+          voluptas, velit autem</p>
+          <h3>jhon deo</h3>
+          <span>traveler</span>
+          <img src="images/pic-1.jpg" alt="">
+      </div>
+
+      <div class="swiper-slide slide">
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          Nam illo repudiandae amet earum neces velit reiciendis aut,  
+          maxime assumenda asperiores ipsam quam. Molestias, tempore.</p>
+          <h3>jhon deo</h3>
+          <span>traveler</span>
+          <img src="images/pic-2.jpg" alt="">
+      </div>
+
+      <div class="swiper-slide slide">
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+          Voluptatem aliquam ratione mollitia deserunt similique 
+          voluptas, velit autem</p>
+          <h3>jhon deo</h3>
+          <span>traveler</span>
+          <img src="images/pic-3.jpg" alt="">
+      </div>
+
+      <div class="swiper-slide slide">
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+          Voluptatem aliquam ratione mollitia deserunt similiq</p>
+          <h3>jhon deo</h3>
+          <span>traveler</span>
+          <img src="images/pic-2.jpg" alt="">
+      </div>
+
+      <div class="swiper-slide slide">
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+          Voluptatem aliquam ratione mollitia deserunt similique 
+          voluptas, velit autem ur adipisicing elit. Voluptatem aliquam ratione </p>
+          <h3>jhon deo</h3>
+          <span>traveler</span>
+          <img src="images/pic-1.jpg" alt="">
+      </div>
+
+      <div class="swiper-slide slide">
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+          Voluptatem aliquam ratione mollitia deserunt similique 
+          voluptas, velit Voluptatem aliquam ratione </p>
+          <h3>jhon deo</h3>
+          <span>traveler</span>
+          <img src="images/pic-3.jpg" alt="">
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts -->
+
+<section class="footer">
+
+  <div class="box-container">
+
+    <div class="box">
+      <h3>quick links</h3>
+      <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+      <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+      <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+      <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+    </div>
+
+    <div class="box">
+      <h3>extra links</h3>
+      <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+      <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+      <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+      <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+    </div>
+
+    <div class="box">
+      <h3>contact info</h3>
+      <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+      <a href="#"> <i class="fas fa-phone"></i> +234-765-9876 </a>
+      <a href="#"> <i class="fas fa-envelope"></i> mehek@gmail.com </a>
+      <a href="#"> <i class="fas fa-map"></i> dharwad, karnataka - 581103 </a>
+    </div>
+
+    <div class="box">
+      <h3>follow us</h3>
+      <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+      <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+      <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+      <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+    </div>
+
+  </div>
+
+  <div class="credit"> created by <span>mrs. Mehek Sayed</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends  -->
+
+
+
+
+
+
+
+
+
+
+
+<!-- swiper js link -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+
+<!-- custom js file link -->
+<script src="script.js"></script>
+
+</body>
+</html>
